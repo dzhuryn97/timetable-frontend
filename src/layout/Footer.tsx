4cddb91b28abbd49c15ec4f57417c52c075cce2a
Volodymyr Dzhuryn <volodymyr.dzhuryn@otakoyi.com>
2024-11-27 20:49:08 +0200

    update config

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,6 +1,7 @@
 import { Text } from "@chakra-ui/react";
 
-export default function Footer() {return (
+export default function Footer() {
+  return (
     <footer
       style={{
         marginTop: "-30px",
